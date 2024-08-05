74a486a71612a0424ef6995eee23fb05249b2e88
Yaroslav Erohin <ysoftware@yandex.ru>
2024-08-05 22:11:55 +0200

    if it compiles, it works!

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
-use minreq;
-
 fn main() {
     println!("Hello world!\n");
-    let response = make_http_request();
+    let _response = make_http_request();
+    parse_entities();
 }
 
-fn make_http_request() -> Result<String, Error> {
-    let response = minreq::get("https://whoniverse-app.com/calcal/main.php")
-        .send().map_err(|_e| {
-            Error::InvalidResponse(0)
+fn make_http_request<'a >() -> Result<String, Error> {
+   let string_value = minreq::get("https://whoniverse-app.com/calcal/main.php")
+        .send()
+        .map_err(|_e| {
+            Error::InvalidResponse
         })?
-        .as_str().map_err(|_e| {
+        .as_str()
+        .map_err(|_e| {
             Error::ExpectedEOF
-        });
+        })
+        ?.to_owned();
 
-    response
+    Ok(string_value)
 }
 
 fn parse_entities() {
-    
+   todo!() 
 }
 
 enum Error {
-    InvalidResponse(i32),
-    ExpectedEntry,
+    InvalidResponse, // (i32),
     ExpectedEOF,
-    ExpectedFoodItem, 
-    ExpectedCalorieValue,
-    InvalidQuantity,
-    InvalidCaloriesMissingKcal,
-    InvalidCalories,
+    // ExpectedEntry,
+    // ExpectedFoodItem, 
+    // ExpectedCalorieValue,
+    // InvalidQuantity,
+    // InvalidCaloriesMissingKcal,
+    // InvalidCalories,
 }
