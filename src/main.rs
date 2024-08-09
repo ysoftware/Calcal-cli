0c9dbb75357d16535a67c94fb7118c7f5b1995b8
Yaroslav Erohin <ysoftware@yandex.ru>
2024-08-09 12:54:04 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![allow(dead_code)] // todo: remove
 #![allow(unused_imports)] // todo: remove
+#![allow(unused_variables)] // todo: remove
 
 use std::process::exit;
 use std::fmt;
 
 fn main() {
     test_advance_if_possible_after_unicode();
+    test_next_matches_ascii();
+    test_first_index();
     println!("---- start program ------");
 
     println!("Hello world!\n");
@@ -16,7 +19,7 @@ fn main() {
     });
 
     parse_entities(response).unwrap_or_else(|error| {
-        eprintln!("An error occured while making http request: {error}");
+        eprintln!("An error occured while parsing response: {error}");
         exit(1)
     });
 }
@@ -29,71 +32,86 @@ fn parse_entities(string: String) -> Result<Vec<EntryEntity>, Error> {
         entries: vec![],
     };
 
-    println!("first tokens: \n{}", parser.text[parser.i..parser.i + 50].to_string());
-
     while parser.i < parser.text.len() {
-            eat_whitespaces_and_newlines(&mut parser);
+        eat_whitespaces_and_newlines(&mut parser);
 
-            if !next_matches_ascii(&parser, "Date: ") {
-                // print_error_position();
-                return Err(Error::ExpectedEntry)
-            }
+        if !next_matches_ascii(&parser, "Date: ") {
+            print_error_position(&parser);
+            return Err(Error::ExpectedEntry)
+        }
 
+        // guard textRemainder[i..<endIndex].starts(with: "Date: "),
+        //       let indexAfterDate = textRemainder.firstIndex(of: " ")
+        // else {
+        //     printErrorPosition()
+        //     throw Error.expectedEntry
+        // }
+        // advance_if_possible_after_unicode(after: indexAfterDate)
 
-            // guard textRemainder[i..<endIndex].starts(with: "Date: "),
-            //       let indexAfterDate = textRemainder.firstIndex(of: " ")
-            // else {
-            //     printErrorPosition()
-            //     throw Error.expectedEntry
-            // }
-            // advance_if_possible_after_unicode(after: indexAfterDate)
-            
-            // eatWhitespaces()
-            // guard let dateNewLineIndex = textRemainder.firstIndex(of: "\n")
-            // else {
-            //     printErrorPosition()
-            //     throw Error.expectedEOF
-            // }
-            // let dateString = String(textRemainder[i..<dateNewLineIndex]).trimmingCharacters(in: .whitespaces)
-            // advance_if_possible_after_unicode(after: dateNewLineIndex)
-            
-            // var sections: [EntryEntity.Section] = []
-            
+        // eatWhitespaces()
+        // guard let dateNewLineIndex = textRemainder.firstIndex(of: "\n")
+        // else {
+        //     printErrorPosition()
+        //     throw Error.expectedEOF
+        // }
+        // let dateString = String(textRemainder[i..<dateNewLineIndex]).trimmingCharacters(in: .whitespaces)
+        // advance_if_possible_after_unicode(after: dateNewLineIndex)
+
+        // var sections: [EntryEntity.Section] = []
 
 
 
-            // for debugging
-            parser.i += 1;
+
+        // for debugging
+        parser.i += 1;
     }
 
 
     return Err(Error::InvalidResponse)
 }
 
-fn next_matches_ascii(parser: &Parser, search: &str) -> bool {
-    let remaining_length = parser.end_index - parser.i;
+fn first_index(parser: &mut Parser, search: char) -> usize {
+    first_index_s(parser.text.as_bytes(), parser.i, parser.end_index, search)
+}
+fn first_index_s(bytes: &[u8], offset: usize, end_index: usize, search: char) -> usize {
+    let mut i = offset;
+    while end_index > i && bytes[i] as char != search {
+        advance_if_possible_after_unicode_s(bytes, &mut i, end_index, 0);
+    }
+    return i
+}
 
-    if remaining_length < search.len() {
+fn next_matches_ascii(parser: &Parser, search: &str) -> bool {
+    next_matches_ascii_s(parser.text.as_bytes(), parser.i, parser.end_index, search)
+}
+fn next_matches_ascii_s(bytes: &[u8], i: usize, end_index: usize, search: &str) -> bool {
+    let remaining_length = end_index - i;
+
+    let search_length: usize = search.len();
+    if remaining_length < search_length {
         return false
     }
 
-    // [parser.i..parser.i + search.len()]
-
-    let byte_slice = parser.text.as_bytes();
+    let byte_slice = &bytes[i..search_length];
     return byte_slice == search.as_bytes()
+}
+
+fn print_error_position(parser: &Parser) {
+    let previous_symbols = &parser.text[parser.i..std::cmp::min(parser.i + 100, parser.end_index)];
+    println!("Parser: Error occured right before this text:\n{}", previous_symbols);
 }
 
 fn eat_whitespaces_but_no_newlines(parser: &mut Parser) {
     while parser.end_index > parser.i && is_whitespace_but_not_newline(parser) {
         let i_value = parser.i.clone(); // bad code, how to write it inline?
-        advance_if_possible_after_unicode(parser.text.as_bytes(), &mut parser.i, parser.end_index, i_value);
+        advance_if_possible_after_unicode(parser, i_value);
     }
 }
 
 fn eat_whitespaces_and_newlines(parser: &mut Parser) {
     while parser.end_index > parser.i && is_whitespace(parser) {
         let i_value = parser.i.clone();
-        advance_if_possible_after_unicode(parser.text.as_bytes(), &mut parser.i, parser.end_index, i_value);
+        advance_if_possible_after_unicode(parser, i_value);
     }
 }
 
@@ -107,36 +125,38 @@ fn is_whitespace(parser: &Parser) -> bool {
     return c == ' ' || c == '\n'
 }
 
-fn advance_if_possible_after_unicode(text: &[u8], i: &mut usize, end_index: usize, after: usize) {
+fn advance_if_possible_after_unicode(parser: &mut Parser, after: usize) {
+    advance_if_possible_after_unicode_s(parser.text.as_bytes(), &mut parser.i, parser.end_index, after);
+}
+fn advance_if_possible_after_unicode_s(text: &[u8], i: &mut usize, end_index: usize, after: usize) {
     if end_index < after {
         *i = end_index;
         return
     }
 
-    *i += 1;
-    let mut count = 1;
+    let mut characters_count = 0;
 
     while *i < end_index {
-        if count == after {
+        if characters_count > after {
             break
         }
 
         let first_byte = text[*i];
-        let advance_i_by: usize;
+        let advance_bytes: usize;
         
         // figure out how many bytes long is this unicode character
         if first_byte <= 0b01111111 {
-            advance_i_by = 1;
+            advance_bytes = 1;
         } else if first_byte <= 0b11011111 {
-            advance_i_by = 2;
+            advance_bytes = 2;
         } else if first_byte <= 0b11101111 {
-            advance_i_by = 3;
+            advance_bytes = 3;
         } else {
-            advance_i_by = 4;
+            advance_bytes = 4;
         }
 
-        count += 1;
-        *i += advance_i_by;
+        characters_count += 1;
+        *i += advance_bytes;
     }
 }
 
@@ -212,14 +232,14 @@ fn make_http_request() -> Result<String, Error> {
 // unit tests
 
 fn test_advance_if_possible_after_unicode() {
-    let string = "über-человек";
+    let string = "üabc";
     let bytes = string.as_bytes();
-    let mut i = 0;
 
-    let diff: usize = 10;
-    let expected = 16;
+    let mut i = 0;
+    let after = 2;
+    let expected = 4;
     
-    advance_if_possible_after_unicode(bytes, &mut i, bytes.len(), diff);
+    advance_if_possible_after_unicode_s(bytes, &mut i, bytes.len(), after);
     
     let test = i == expected;
     if test {
@@ -229,3 +249,29 @@ fn test_advance_if_possible_after_unicode() {
     }
 }
 
+fn test_next_matches_ascii() {
+    let string1 = "mensch";
+    let test1 = next_matches_ascii_s(string1.as_bytes(), 0, string1.len(), "mensch");
+
+    let string2 = "übermensch";
+    let test2 = next_matches_ascii_s(string2.as_bytes(), 0, string2.len(), "mensch");
+    
+    if test1 && !test2 {
+        println!("Test 2: OK");
+    } else {
+        println!("Test 2: FAIL!");
+    }
+}
+
+fn test_first_index() {
+    let string = "übermensch bin ich";
+    let index = first_index_s(string.as_bytes(), 0, string.len(), ' ');
+
+    let expected = 11;
+    let test = index == expected;
+    if test {
+        println!("Test 3: OK");
+    } else {
+        println!("Test 3: FAIL! i: {}, expected {}", index, expected);
+    }
+}
